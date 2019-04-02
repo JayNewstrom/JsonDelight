07548fd7a83f702f5faea76fb37c4c515dad811f
Jay Newstrom <jaynewstrom@gmail.com>
2019-04-02 14:29:44 -0500

    Properly register kotlin sources. (#90)
    I noticed this was broken when trying newer versions of the kotlin gradle plugin. This fixes it :)

--- plugin/gradle-plugin/src/main/kotlin/com/jaynewstrom/jsonDelight/gradle/JsonDelightPlugin.kt
+++ plugin/gradle-plugin/src/main/kotlin/com/jaynewstrom/jsonDelight/gradle/JsonDelightPlugin.kt
@@ -2,8 +2,10 @@ package com.jaynewstrom.jsonDelight.gradle
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.api.AndroidSourceSet
 import com.android.build.gradle.api.BaseVariant
 import com.jaynewstrom.jsonDelight.compiler.VERSION
 import org.gradle.api.DomainObjectSet
@@ -13,21 +15,30 @@ import org.gradle.api.artifacts.DependencyResolutionListener
 import org.gradle.api.artifacts.DependencySet
 import org.gradle.api.artifacts.ResolvableDependencies
 import org.gradle.api.artifacts.UnknownConfigurationException
+import org.gradle.api.file.SourceDirectorySet
+import org.gradle.api.internal.HasConvention
+import org.jetbrains.kotlin.gradle.plugin.KOTLIN_DSL_NAME
+import org.jetbrains.kotlin.gradle.plugin.KOTLIN_JS_DSL_NAME
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import java.io.File
 
 class JsonDelightPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.plugins.all {
             when (it) {
-                is AppPlugin -> configureAndroid(project,
-                    project.extensions.getByType(AppExtension::class.java).applicationVariants)
-                is LibraryPlugin -> configureAndroid(project,
-                    project.extensions.getByType(LibraryExtension::class.java).libraryVariants)
+                is AppPlugin -> {
+                    val extension = project.extensions.getByType(AppExtension::class.java)
+                    configureAndroid(project, extension, extension.applicationVariants)
+                }
+                is LibraryPlugin -> {
+                    val extension = project.extensions.getByType(LibraryExtension::class.java)
+                    configureAndroid(project, extension, extension.libraryVariants)
+                }
             }
         }
     }
 
-    private fun <T : BaseVariant> configureAndroid(project: Project, variants: DomainObjectSet<T>) {
+    private fun <T : BaseVariant> configureAndroid(project: Project, androidExtension: BaseExtension, variants: DomainObjectSet<T>) {
         project.extensions.create("jsonDelight", JsonDelightExtension::class.java)
 
         val compileDeps = project.dependencySetForName("implementation") ?: project.dependencySetForName("compile")
@@ -40,6 +51,8 @@ class JsonDelightPlugin : Plugin<Project> {
             override fun afterResolve(dependencies: ResolvableDependencies?) {
             }
         })
+
+        val sources: Map<String, SourceDirectorySet> = androidExtension.sources()
 
         variants.all { variant ->
             val buildDirectory = listOf("generated", "source", "jsonDelight", variant.name).fold(project.buildDir, ::File)
@@ -55,6 +68,8 @@ class JsonDelightPlugin : Plugin<Project> {
                 task.include("**/*.json")
             }
 
+            sources[variant.name]?.srcDir(buildDirectory.toRelativeString(project.projectDir))
+
             // TODO: https://issuetracker.google.com/issues/117343589
             variant.registerJavaGeneratingTask(taskProvider.get(), buildDirectory)
         }
@@ -67,4 +82,23 @@ class JsonDelightPlugin : Plugin<Project> {
             null
         }
     }
+
+    private fun BaseExtension.sources(): Map<String, SourceDirectorySet> {
+        return sourceSets
+            .associate { sourceSet ->
+                sourceSet.name to (sourceSet as AndroidSourceSet).kotlinSourceSet!!
+            }
+    }
+
+    // Copied from kotlin plugin
+    private val Any.kotlinSourceSet: SourceDirectorySet?
+        get() {
+            val convention = (getConvention(KOTLIN_DSL_NAME) ?: getConvention(KOTLIN_JS_DSL_NAME)) ?: return null
+            val kotlinSourceSetIface =
+                convention.javaClass.interfaces.find { it.name == KotlinSourceSet::class.qualifiedName }
+            val getKotlin = kotlinSourceSetIface?.methods?.find { it.name == "getKotlin" } ?: return null
+            return getKotlin(convention) as? SourceDirectorySet
+        }
+
+    private fun Any.getConvention(name: String): Any? = (this as HasConvention).convention.plugins[name]
 }
